7af665a47cbd92d55dad3e9b332d2557746f41a3
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-06-21 00:34:06 +0530

    UI added for FormInput

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
+import React,{Fragment} from 'react';
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Fragment>
+      <Header>
+      </Header>
+      <main>
+          <Meals />
+        </main>
+      </Fragment>
   );
 }
 
